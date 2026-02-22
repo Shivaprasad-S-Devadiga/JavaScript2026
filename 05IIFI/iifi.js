5bcea1c76264e3aa95d6c717b36fa62ced676977
shivaprasadad <shivaprasadad@BMDVSHIVAPRASADAD.local>
2026-02-23 01:37:15 +0530

    iifi

---
+++ 05IIFI/iifi.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// usedto remove global noise
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
